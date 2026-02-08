b0be4824f5575aba11132d15e95c49dc4ca5bf8a
Ajaykumar661 <ajaykumar661k@gmail.com>
2026-02-08 22:20:49 +0530

    changed the popup text

--- pixel-love-quiz/app/page.tsx
+++ pixel-love-quiz/app/page.tsx
@@ -393,7 +393,7 @@ export default function Home() {
               className="font-pixel text-xs mt-8 text-black"
               style={{ textShadow: '1px 1px 0px rgba(255, 255, 255, 0.5)' }}
             >
-              (The "No" button is shy 😊)
+              No amuki paaru, nee dhan dhairiyamana aal aache!
             </motion.p>
           </motion.div>
         </motion.div>
